999217c09e8a5c4bdea8f720b1b1d54479239381
Eddy Núñez <eddy.gerardo@gmail.com>
2018-10-27 16:57:52 -0600

    Update tinyerp-fundatec/controller/Assets/AssetsController.php

--- tinyerp-fundatec/controller/Assets/AssetsController.php
+++ tinyerp-fundatec/controller/Assets/AssetsController.php
@@ -1,38 +1,49 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class AssetsController extends controller{
-       
+class AssetsController extends controller {
+
     function __construct() {
         parent::__construct(MODULO_ASSETS);
         Sesion::open();
         $this->verifyActiveSession();
         $this->loadModelAssets("MActivos");
         //$this->loadModelContract("indexModel");
-    } 
-    
-     public function procesar() {
+    }
+
+    public function procesar() {
         $action = $this->getAction();
+
         switch ($action) {
             case 'nuevo':
-            $this->runView("frmActivos", "assets/index");                
-            break;
+                $this->runView("frmNewAsset", "assets/index");
+                break;
+            case 'editAsset':
+                $this->runView("frmEditAsset", "assets/index");
+                break;
+            case 'newRepair':
+                $this->runView("frmNewRepair", "assets/index");
+                break;
             case 'requestAssets':
-                $assetsModel = new MActivos();    
+                $assetsModel = new MActivos();
                 $databaseResult = $assetsModel->getAllAssets();
-                echo ($databaseResult);                    
-            break;
+                echo ($databaseResult);
+                break;
+            case 'getAllCategoryAssest':
+                $assetsModel = new MActivos();
+                $getCategoryAssestResp = $assetsModel->getAllCategoryAssest();
+                echo ($getCategoryAssestResp);
             default :
                 $this->showAssetsIndex();
                 break;
         }
     }
-    
+
 //    public function procesar(){
 //       if (isset($_POST["action"])) {        
 //           switch ($this->getAction("action")) { 
@@ -55,18 +66,15 @@ class AssetsController extends controller{
 //          $this->showAssetsIndex();
 //       }
 //    }
-    
-    protected function showAssetsIndex(){
+
+    protected function showAssetsIndex() {
         //$indexModelAux = new indexModel();
-       // $this->CashRegister = $indexModelAux->getCashRegisterByUser();
+        // $this->CashRegister = $indexModelAux->getCashRegisterByUser();
         $this->runView("gestionActivos");
     }
-    
-    public  function mostrarFrmActivos(){
+
+    public function mostrarFrmActivos() {
         $this->runView("frmActivos");
     }
-    
 
 }
-
-
